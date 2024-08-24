13c092f88354e3e7e9887964077f40819f558383
Valentina96-ER <tinaencireg2014@gmail.com>
2024-08-24 17:08:44 -0300

    Actualizaciones comentarios

--- workspace-inicial/js/login.js
+++ workspace-inicial/js/login.js
@@ -4,8 +4,8 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    // Aquí deberías realizar la validación con tu backend
-    // Este es solo un ejemplo simple
+// agregue funcion que redireccione al sitio portada cuando se presiona INGRESAR
+// en el caso de no completar un campo le sale el alert de abajo
     if (username !== "" && password !== "") {
         localStorage.setItem('isLoggedIn', 'true');
         window.location.href = 'index.html';
