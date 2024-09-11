861089f8456a6f209d312839ceae36d53ef0a2e5
Valentina96-ER <tinaencireg2014@gmail.com>
2024-09-11 18:23:13 -0300

    cambios entrega 2 ultimos

--- workspace-inicial/js/index.js
+++ workspace-inicial/js/index.js
@@ -23,4 +23,18 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('logoutButton').addEventListener('click', function() {
     localStorage.removeItem('isLoggedIn');
     window.location.href = 'login.html';
+    localStorage.removeItem('username');
 });
+ // desafiate
+document.addEventListener('DOMContentLoaded', function() {
+    // Recupera el nombre del usuario de localStorage
+    const username = localStorage.getItem('username');
+  
+    if (username) {
+      // Selecciona el enlace donde se mostrará el nombre del usuario
+      const usernameDisplay = document.getElementById('username-display');
+      usernameDisplay.textContent = `${username}`;
+    } else {
+      console.log('No se encontró un nombre de usuario en localStorage.');
+    }
+  });
